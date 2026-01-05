0f0bd013ca5bdbbcf9409e3ff363f32a30005a24
TALISSON ALMEIDA <talissonalmeida11@gmail.com>
2026-01-05 06:55:36 -0300

    ajustando horario

--- script.js
+++ script.js
@@ -177,7 +177,7 @@ checkoutBtn.addEventListener("click", function(){
 function checkRestaurantOpen(){
     const data = new Date();
     const hora = data.getHours();
-    return hora >= 18 && hora < 22;
+    return hora >= 06 && hora < 22;
 }
 
 const spanItem = document.getElementById("date-span")
